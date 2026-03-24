3a211dd934c24215ca378d932e0ed571fe819ea7
EzeMartino <ezequielrmartino@gmail.com>
2026-03-24 14:38:58 -0300

    Caching metadata and pipeline on app startup using lifespan and endpoints updates

--- src/api/main.py
+++ src/api/main.py
@@ -1,7 +1,13 @@
+from contextlib import asynccontextmanager
+from functools import lru_cache
+import json
+
 from fastapi import FastAPI, HTTPException
+import joblib
 from pydantic import BaseModel, ConfigDict
 from typing import Optional
 
+from src.config import get_active_metadata_path, get_active_pipeline_path
 from src.models.predict import predict_single
 
 
@@ -40,9 +46,37 @@ class FraudInput(BaseModel):
 
     model_config = ConfigDict(extra="forbid")
 
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    # Startup
+    app.state.pipeline = get_pipeline()
+    app.state.metadata = get_metadata()
+    yield
+    # Shutdown (opcional)
 
-app = FastAPI(title="Fraud Detection API")
+app = FastAPI(title="Fraud Detection API", lifespan=lifespan)
 
+@lru_cache(maxsize=1)
+def get_pipeline():
+    pipeline_path = get_active_pipeline_path()
+    if not pipeline_path.exists():
+        raise FileNotFoundError(
+            f"Model artifact not found at {pipeline_path}. "
+            "Run 'train_random_forest.py' to generate model artifacts first"
+        )
+    return joblib.load(pipeline_path)
+
+@lru_cache(maxsize=1)
+def get_metadata():
+    metadata_path = get_active_metadata_path()
+    if not metadata_path.exists():
+        raise FileNotFoundError(
+            f"Model artifact not found at {metadata_path}. "
+            "Run 'train_random_forest.py' to generate model artifacts first"
+        )
+        
+    with open(metadata_path, "r", encoding="utf-8") as f:
+        return json.load(f)
 
 @app.get("/")
 def root():
@@ -50,17 +84,30 @@ def root():
 
 @app.get("/health")
 def health():
-    return {"status": "ok"}
+    return {
+        "status" : "ok",
+        "model_loaded": True,
+        "model_version": app.state.metadata["model_version"]
+        }
 
 @app.post("/predict")
 def predict(payload: FraudInput):
     try:
         data = payload.model_dump()
         threshold = data.pop("threshold", 0.5)
-        result = predict_single(data, threshold=threshold)
         
         if not 0 <= threshold <= 1:
             raise ValueError("Threshold must be between 0 and 1.")
+        
+        pipeline = app.state.pipeline
+        metadata = app.state.metadata
+        
+        result = predict_single(
+            data=data, 
+            pipeline=pipeline, 
+            metadata=metadata, 
+            threshold=threshold
+            )
         
         return result
     except ValueError as e:
